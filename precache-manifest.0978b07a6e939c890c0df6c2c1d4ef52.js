d609f4b7bbaecdfcf8807be189391340dde63576
Yurii Rysyn <zerocool_cs@ukr.net>
2020-09-02 11:16:17 +0300

    Updates

---
+++ precache-manifest.0978b07a6e939c890c0df6c2c1d4ef52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd73936400a2e59e8a556bcf3a4a357",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7040559ec84714d19d8",
+    "url": "./static/css/main.ba2eb5b1.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7040559ec84714d19d8",
+    "url": "./static/js/main.53b5ae59.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.0978b07a6e939c890c0df6c2c1d4ef52.js"
 );
 
 self.addEventListener('message', (event) => {
